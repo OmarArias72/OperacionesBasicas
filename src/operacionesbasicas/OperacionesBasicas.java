02bdaae6ca023dd6ac68ab5dfc90d3410e21bd78
Omar <omar.arias357@gmail.com>
2023-02-23 17:47:18 -0600

    creacion de metodos para operaciones y pase de argumentos con parametros

--- src/operacionesbasicas/OperacionesBasicas.java
+++ src/operacionesbasicas/OperacionesBasicas.java
@@ -12,31 +12,41 @@ package operacionesbasicas;
 
 public class OperacionesBasicas {
     
-    int valor1 = 10, valor2 = 8;
+    //int valor1 = 10, valor2 = 8;
     float suma, resta, multiplicación, division;
 
-    public void Operaciones(){
+    public void suma(int valor1, int valor2){
         
         //Reliza el cálculo de la suma
         suma= valor1 + valor2;
         System.out.println("La suma es: " + suma);
-        
+
+    }
+    public void resta (int valor1, int valor2){
         //Reliza el cálculo de la resta
         resta= valor1 - valor2;
         System.out.println("La resta es: " + resta);
-        
+}
+    public void multiplicacion(int valor1, int valor2){
         //Reliza el cálculo de la multiplicación
         multiplicación = valor1 * valor2;
         System.out.println("La multiplicación es: " + multiplicación);
-        
+    }
+    public void division(int valor1, int valor2){
         //Reliza el cálculo de la división
         division = valor1 / valor2;
         System.out.println("La division es: " + division);
     }
+            
+            
 
+        
     public static void main(String[] args) {        
         OperacionesBasicas operaciones = new OperacionesBasicas();
-        operaciones.Operaciones();
+        operaciones.suma(10,8);
+        operaciones.resta(10,8);
+        operaciones.multiplicacion(10,8);
+        operaciones.division(10,8);
     }
     
 }
